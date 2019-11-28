687917de54dbffa08362f8719747bcad5a284aee
alan-serrano <alan.serrano.pch@gmail.com>
2019-11-28 11:29:06 -0400

    Funcionalidad optimización png Optimización código

--- optimizar_imagen.cmd
+++ optimizar_imagen.cmd
@@ -8,6 +8,10 @@ endlocal
 
 pause
 
+REM Añadiendo binarios de programas para optimizar las imágenes png
+set PNG_QUANT=C:\pngquant\pngquant
+set PNG_CRUSH=C:\pngcrush\pngcrush
+
 :cambiar_nombre
 
     set /p cambiarNombre=Desea cambiar y enumerar el nombre de las imagenes? ^(y/n^):
@@ -29,8 +33,23 @@ pause
 
 :fin_cambiar_nombre
 
+
+:optimizar_imagen
+    set /p optimizar_imagen=Desea optimizar los archivos de imagen? ^(y/n^):
+
+    IF /i %optimizar_imagen% EQU y (
+        goto :fin_optimizar_imagen
+    )
+
+    IF /i %optimizar_imagen% EQU n (
+        goto :fin_conversion_a_jpg
+    )
+
+    goto :optimizar_imagen
+:fin_optimizar_imagen
+
 :conversion_a_jpg
-    set /p convertir_imagen=Desea convertir todo a jpg? ^(y/n^):
+    set /p convertir_imagen=Desea convertir todos los archivos de imagen a jpg? ^(y/n^):
 
     IF /i %convertir_imagen% EQU y (
         goto :fin_conversion_a_jpg
@@ -44,16 +63,15 @@ pause
 
 :fin_conversion_a_jpg
 
-
 REM Reseteando los nombres
 set count=0
 FOR /R "./" %%i IN (*) DO (
     IF /i %%~xi EQU .jpg (
-        call :subroutine "%%i" namereset no
+        call :subroutine "%%i" namereset false
     )
 
     IF /i %%~xi EQU .png (
-        call :subroutine "%%i" namereset no
+        call :subroutine "%%i" namereset false
     )
 )
 
@@ -61,11 +79,11 @@ REM Cambiando Nombre y optimizando los archivos
 set count=0
 FOR /R "./" %%i IN (*) DO (
     IF /i %%~xi EQU .jpg (
-        call :subroutine "%%i" %fileNametoChange% segundo_ciclo
+        call :subroutine "%%i" %fileNametoChange% true
     )
 
     IF /i %%~xi EQU .png (
-        call :subroutine "%%i" %fileNametoChange% segundo_ciclo
+        call :subroutine "%%i" %fileNametoChange% true
     )
 )
 
@@ -85,23 +103,35 @@ GOTO :eof
     set fileNameChanged=%fileNameChanged%%count%
     set fullNameChanged=%fileNameChanged%%fileExtension%
 
-    set runOptimizacion=%3
+    set es_segundo_ciclo=%3
 
-    IF %runOptimizacion% EQU segundo_ciclo (
-        IF /i %fileExtension% EQU .jpg (
-            mogrify -strip -resize 1750x1600^> -quality 81 -interlace Plane -sampling-factor 4:2:0 -colorspace RGB "%fullName%" -define jpeg:dct-method=float
-        )
+    IF %es_segundo_ciclo% EQU true (
 
-        IF /i %convertir_imagen% EQU y (
-            IF /i %fileExtension% EQU .png (
-                REM Convirtiendo
-                mogrify -format jpg -strip -resize 1750x1600^> -quality 81 -interlace Plane -sampling-factor 4:2:0 -colorspace RGB -define jpeg:dct-method=float "%fullName%"
-                del "%fullName%"
+        IF /i %optimizar_imagen% EQU y (
+            
+            IF /i %fileExtension% EQU .jpg (
+                mogrify -strip -resize 1750x1600^> -quality 81 -interlace Plane -sampling-factor 4:2:0 -colorspace RGB "%fullName%" -define jpeg:dct-method=float
+            )
 
-                REM Cambiando fullName a jpg
+            IF /i %convertir_imagen% EQU y (
+                IF /i %fileExtension% EQU .png (
+                    REM Convirtiendo
+                    mogrify -format jpg -strip -resize 1750x1600^> -quality 81 -interlace Plane -sampling-factor 4:2:0 -colorspace RGB -define jpeg:dct-method=float "%fullName%"
+                    del "%fullName%"
 
-                set fullName=%filePath%%fileName%.jpg
-                set fullNameChanged=%fileNameChanged%.jpg
+                    REM Cambiando fullName a jpg
+
+                    set fullName=%filePath%%fileName%.jpg
+                    set fullNameChanged=%fileNameChanged%.jpg
+                )
+            )
+
+            IF /i %convertir_imagen% EQU n (
+                IF /i %fileExtension% EQU .png (
+                    mogrify -strip -resize 1750x1600^> "%fullName%"
+                    %PNG_QUANT% --quality=45-85 --ext .png --force --strip --skip-if-larger "%fullName%"
+                    %PNG_CRUSH% -warn -ow "%fullName%"
+                )
             )
         )
     )
